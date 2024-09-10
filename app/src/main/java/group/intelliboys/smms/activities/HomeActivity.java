1499f4d8be2e3b375b13ffa0cba3cadc44bd9de1
ZaiNu3x <zainu3x007@gmail.com>
2024-09-10 16:32:29 +0800

    TEST2 2nd commit

--- app/src/main/java/group/intelliboys/smms/activities/HomeActivity.java
+++ app/src/main/java/group/intelliboys/smms/activities/HomeActivity.java
@@ -1,18 +1,63 @@
 package group.intelliboys.smms.activities;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import group.intelliboys.smms.R;
+import group.intelliboys.smms.fragments.HomeFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView
+        .OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+
+
+    @SuppressLint({"CommitTransaction", "ResourceType"})
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.open_nav, R.string.close_nav);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment()).commit();
+
+            navigationView.setCheckedItem(R.layout.fragment_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 
     @Override
@@ -26,4 +71,5 @@ public class HomeActivity extends AppCompatActivity {
                 }))
                 .show();
     }
+
 }
